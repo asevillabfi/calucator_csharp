a69edf82d5f1d04c85fed60e1736282f66bf041d
asevillabfi <alyanna.sevilla@newbrighton.edu.ph>
2024-01-19 11:35:37 +0800

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace CalculatorApplication
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("First Number: ");
+                int firstNum = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Second Number: ");
+                int secondNum = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Select Operator [ +  ,   -   ,   *   ,   /   ]");
+
+                Console.WriteLine("Operator: ");
+                string operation = Console.ReadLine();
+
+                int result = 0;
+
+                if (operation == "+")
+                {
+                    result = firstNum + secondNum;
+                }
+                else if (operation == "-")
+                {
+                    result = firstNum - secondNum;
+                }
+                else if (operation == "*")
+                {
+                    result = firstNum * secondNum;
+                }
+                else if (operation == "/")
+                {
+                    result = firstNum / secondNum;
+                }
+                else
+                {
+                    Console.WriteLine("Try again!");
+                }
+
+                Console.WriteLine("=============");
+                Console.WriteLine(firstNum + " " + operation + " " + secondNum + " = " + result);
+                Console.WriteLine("Continue? (Y = Yes, N = No)");
+
+            } while (Console.ReadLine().ToUpper() == "Y");
+
+            Console.WriteLine("Adios");
+            Console.ReadKey();
+        }
+    }
+}
